0c4ee87e6c31fb808c872573a901ac19ee0d1e78
nkmakau <noshmak@gmail.com>
2021-02-26 14:31:09 +0300

    Added more python codes

---
+++ python/lists.py
@@ -0,0 +1,10 @@
+# a list for pple to show a comment
+names = ["Harry", "Hermione", "Ron", "Ginny"]
+
+print(names[1])
+
+names.append("Draco")
+
+names.sort()
+
+print(names)
---
+++ python/sequences.py
@@ -0,0 +1,12 @@
+names = "Harry", "Ron", "Hermione"
+
+print(names [0])
+
+coordinateX = 10.0
+coordinateY = 20.0
+
+print(coordinateX)
+print(coordinateY)
+
+coordinate = (10.0, 20.0)
+print(coordinate)
